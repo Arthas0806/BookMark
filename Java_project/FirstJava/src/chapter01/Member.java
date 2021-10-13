be496edb4eb733f9ba5215515286ff92925a5844
최종혁 <cardview0806@gmail.com>
2021-10-14 01:04:41 +0900

    Update Member.java

--- Java_project/FirstJava/src/chapter01/Member.java
+++ Java_project/FirstJava/src/chapter01/Member.java
@@ -1,13 +1,7 @@
 package chapter01;
 
 public class Member {
-<<<<<<< HEAD
 	
-=======
-
-
-		
->>>>>>> 7f447c973bf3e0e73d3979a5eb915ed57f018080
 	// 2.Member 클래스를 생성하고, 
 	//   main()메소드를 만들고 자기 자신의 이름을 출력하는 메소드를 만들어 출력해 봅시다.
 	public static void main(String[] args) {
@@ -16,11 +10,7 @@ public class Member {
 		System.out.println("메소드 호출");
 		sayName();
 	}
-<<<<<<< HEAD
 	
-=======
-		
->>>>>>> 7f447c973bf3e0e73d3979a5eb915ed57f018080
 	// 이름을 출력하는 메소드
 	// 반환타입 메소드 이름 (매개변수,...)
 	static void sayName() {
