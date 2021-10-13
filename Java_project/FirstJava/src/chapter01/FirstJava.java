2e5ce46e4e31ded81aeb87abcbb01f7174b1c755
Arthas0806 <92347757+Arthas0806@users.noreply.github.com>
2021-10-13 10:24:24 +0900

    Update FirstJava.java

--- Java_project/FirstJava/src/chapter01/FirstJava.java
+++ Java_project/FirstJava/src/chapter01/FirstJava.java
@@ -1,10 +1,10 @@
 package chapter01;
 
 public class FirstJava {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
-	}
+		System.out.println("First JAVA");
 
-}
+		}
+
+	}
