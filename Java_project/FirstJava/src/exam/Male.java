fd5eb7427d159390d8e19e7754c8e91c69e55c73
Arthas0806 <92347757+Arthas0806@users.noreply.github.com>
2021-10-19 13:19:11 +0900

    Create Male.java

---
+++ Java_project/FirstJava/src/exam/Male.java
@@ -0,0 +1,31 @@
+package exam;
+
+//2.Person 클래스를 상속해서 확장하는 새로운 클래스 Male 클래스와 Female 클래스를 정의 해봅시다.
+public class Male extends Person {
+	
+	int age;
+	
+	
+
+	@Override
+	public void Hello(int name, int age) {
+		
+		super.Hello(name, age);
+	}
+	
+	
+	//①각 클래스는 상속 받은 멤버 외에 추가적인 변수와 메소드를 추가해서 새로운 클래스를 정의해봅시다.
+
+	//②각 클래스는 상속 받은 멤버 외에 추가적인 변수와 메소드를 추가해서 새로운 클래스를 정의해봅시다.
+
+	//③Person 클래스에서 정의된 인사하는 메소드를 오버라이딩 해봅시다.
+
+	//④Person 클래스에 생성자를 정의해서 인스턴스 변수들을 초기화 해봅시다.
+	
+	
+//	3.main()메소드를 정의해봅시다.
+
+//	①Person 클래스를 상속받은 Male클래스와 Female클래스를 이용해서 인스턴스를 생성해 봅시다.
+
+//	②생성된 인스턴스들을 이용해서 메소드를 호출해봅시다.
+}
