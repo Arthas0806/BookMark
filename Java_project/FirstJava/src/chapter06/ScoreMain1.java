8ad524d9554ff807c8b7e8f69d7df7bcd0784800
Arthas0806 <92347757+Arthas0806@users.noreply.github.com>
2021-10-19 09:28:39 +0900

    Update ScoreMain1.java

--- Java_project/FirstJava/src/chapter06/ScoreMain1.java
+++ Java_project/FirstJava/src/chapter06/ScoreMain1.java
@@ -9,18 +9,18 @@ public class ScoreMain1 {
 		manager.insertScore(new Student("king", 90, 80, 70));
 		manager.insertScore(new Student("SCOTT", 78, 60, 69));
 		
-		// 2. 데이터 전체 출력
-		manager.showAllData();
-		
-		// 3. 데이터 검색
-		manager.searchData(/*"손흥민"*/);
-		manager.searchData(/*"이강인"*/);
-		
-		// 4. 데이터 삭제
-		manager.deleteScore(/*"SCOTT"*/);
-		manager.deleteScore(/*"이강인"*/);
-		
-		// 5. 데이터 검색 또는 전체 데이터 출력
-		manager.showAllData();
+//		// 2. 데이터 전체 출력
+//		manager.showAllData();
+//		
+//		// 3. 데이터 검색
+//		manager.searchData(/*"손흥민"*/);
+//		manager.searchData(/*"이강인"*/);
+//		
+//		// 4. 데이터 삭제
+//		manager.deleteScore(/*"SCOTT"*/);
+//		manager.deleteScore(/*"이강인"*/);
+//		
+//		// 5. 데이터 검색 또는 전체 데이터 출력
+//		manager.showAllData();
 	}
 }
