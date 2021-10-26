2acd583491dea1d346aac0d29285b04bb8ef2c59
최종혁 <cardview0806@gmail.com>
2021-10-26 18:22:02 +0900

    10/26

--- Java_project/FirstJava/src/chapter12/circle/ObjectSerializable.java
+++ Java_project/FirstJava/src/chapter12/circle/ObjectSerializable.java
@@ -1,6 +1,9 @@
 package chapter12.circle;
 
+
+
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -9,15 +12,15 @@ import java.util.ArrayList;
 
 public class ObjectSerializable {
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
 		
-		//인스턴스 저장을 위한 스트림
+		// 인스턴스 저장을 위한 스트림
 		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Object.ser"));
-				
+
 		// 인스턴스 저장
 		out.writeObject(new Circle(1, 1, 2.4));
 		out.writeObject(new Circle(2, 2, 4.8));
-		out.writeObject(new Circle("문자열"));
+		out.writeObject(new String("문자열"));
 		
 		ArrayList<String> arr = new ArrayList<String>();
 		arr.add("one");
@@ -29,10 +32,13 @@ public class ObjectSerializable {
 		out.close();
 		
 		// 저장된 파일을 이용해서 인스턴스를 생성 -> 복원
+		// 복원시에는 저장 순서에 맞게 복원해주어야 한다!!!
 		ObjectInputStream in = new ObjectInputStream(new FileInputStream("Object.ser"));
 		
+		//String str = (String) in.readObject();
+		
 		// 복원 : 인스턴스 생성하고 반환
-		Circle c1 = (Circle) in.readObject(); // ClassNotFoundException -> 경로에 존재하지 않으면 이게 일어나서 처리
+		Circle c1 = (Circle) in.readObject();
 		Circle c2 = (Circle) in.readObject();
 		
 		String str = (String) in.readObject();
@@ -50,5 +56,10 @@ public class ObjectSerializable {
 		System.out.println(str);
 		
 		
+		
+		
 	}
+
+
+	
 }
