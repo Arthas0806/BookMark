a06343a7013f45fdf70bf9efa01b14817a088f2a
Arthas0806 <92347757+Arthas0806@users.noreply.github.com>
2021-11-08 17:56:37 +0900

    1108

--- Java_project/FirstJava/src/exam/Ex08CalculatorMain.java
+++ Java_project/FirstJava/src/exam/Ex08CalculatorMain.java
@@ -6,7 +6,7 @@ public class Ex08CalculatorMain {
 		long n1 = 987654;
 		long n2 = 123456;
 
-		ExCalculator cal = new ExCalculatorImple();
+		Ex08Calculator cal = new Ex08Calculator();
 		
 		System.out.println(n1 + " + " + n2 + " = " + cal.add(n1, n2));
 		System.out.println(n1 + " - " + n2 + " = " + cal.substract(n1, n2));
